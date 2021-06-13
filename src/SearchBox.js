f1272a01194dd838d0806f6f54b8a5f401f8844e
Aryan Sharma <aryan.rakesh.121@gmail.com>
2021-06-13 11:59:27 +0530

    Starting Robo Friends

---
+++ src/SearchBox.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const SearchBox = ({onSearchChange}) => {
+    return(
+        
+        <input className='pa3 ba b--green bg-lightest-blue' type = 'search' placeholder = 'Search Robots' onChange={onSearchChange}/>
+    );
+}
+
+export default SearchBox;
